ff11b72dbde86055b627078f563008800d83f675
masumpanchal <110961352+masumpanchal@users.noreply.github.com>
2022-09-20 12:49:26 +0530

    Add files via upload

---
+++ exercise1.java
@@ -0,0 +1,33 @@
+public class exercise1 {
+    public static void main(String[] args) {
+
+        int a;
+
+        System.out.print("(" );
+        for (a=3; a<=6; a++){
+            System.out.print( a*a +" , ");
+        }
+        System.out.println(")");
+
+
+        System.out.print("(");
+        for (a=3; a<=5; a++){
+            System.out.print( a*a +" , ");
+        }
+        System.out.println(")");
+
+        System.out.print("(");
+        for (a=3; a<=4; a++){
+            System.out.print( a*a +" , ");
+        }
+        System.out.println(")");
+
+        System.out.print("(");
+        for (a=3; a<=3; a++){
+            System.out.print(a*a);
+        }
+        System.out.println(")");
+
+    }
+
+}
