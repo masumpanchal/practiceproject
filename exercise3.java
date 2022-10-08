2ffc777e889eae66412cb2daca6ff78be7af2848
masumpanchal <110961352+masumpanchal@users.noreply.github.com>
2022-10-08 22:53:31 +0530

    Add files via upload

--- exercise3.java
+++ exercise3.java
@@ -1,37 +1,38 @@
 import java.util.Arrays;
 
 public class exercise3 {
-    public static void main(String[] args){
-        int a[]= new int[]{3, 4, 5, 6};
-        int b[]= new int[]{1, 2, 3, 4};
-        int c[]= new int[a.length+ b.length];
-        int d[]= new int[15];
-        int j=0, k=0;
+    public static void main(String[] args) {
+        int a[] = new int[]{3, 4, 5, 6};
+        int b[] = new int[]{1, 2, 3, 4};
+        int c[] = new int[a.length + b.length];
+        int n[] = new int[a.length * b.length];
+        int sum0 = 0;
+        int sum1 = 0;
+        int sum2 = 0;
+        int sum3 = 0;
 
-        for (int i=0; i<c.length; i++){
-            if (i%2 == 0)
-            c[i]=a[j++];
+
+        int j = 0, k = 0;
+
+        for (int i = 0; i < c.length; i++) {
+            if (i % 2 == 0)
+                c[i] = a[j++];
             else
-            c[i]=b[k++];
+                c[i] = b[k++];
         }
-        for (int i=0; i< c.length; i++){
+        for (int i = 0; i < c.length; i++) {
             System.out.print(c[i] + " ");
         }
         System.out.println();
 
-        for (int i=0; i<d.length; i++){
-            if ()
+        for (int i = 0; i < a.length; i++) {
+            sum0 = a[i] + b[0];
+            sum1 = a[i] + b[1];
+            sum2 = a[i] + b[2];
+            sum3 = a[i] + b[3];
 
-
+            System.out.print(sum0 + " "+ sum1 + " "+sum2 + " "+sum3 + " ");
+            }
         }
-        /*System.out.print(a[0]+b[0]+" ");
-        System.out.print(a[1]+b[0]+" ");
-        System.out.print(a[2]+b[0]+" ");
-        System.out.print(a[3]+b[0]+" ");*/
-
-
-
     }
-
-}
 
